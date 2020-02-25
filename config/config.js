59ba18d1c50da460188f3a19aa2c0c591abcc28f
江伟业 <jiangweiye@outlook.com>
2020-02-25 11:42:33 +0800

    修改服务器路径

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports =  {
   baseUrl:{
-    localhost:'http://localhost:3000',////前端 本地 -> 本地服务器
-    localhost_http:'http://s25.natfrp.com:35098',//前端 本地  -> 服务器远端
+    localhost:'http://localhost:3001',////前端 本地 -> 本地服务器
+    localhost_http:'http://34.92.142.41:80',//前端 本地  -> 服务器远端
     localhost_https:'https://uncle9.top',//前端本地 -> 服务器远端(https)
   }
 }
